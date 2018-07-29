65d12597f8851d371df21b7995f04ca99fc6d396
TT <823232162@qq.com>
2018-07-29 20:16:40 +0800

    updata

--- novel_search/models.py
+++ novel_search/models.py
@@ -18,7 +18,7 @@ class NovelInfo(models.Model):
     book_url = models.CharField(max_length=100, null=True, db_index=True)
     image = models.CharField(max_length=100, null=True, db_index=True)
 
-    # 外键
+    # # 外键
     source = models.ForeignKey(to="NovelSource", to_field="uid", on_delete=models.CASCADE, default=1)
 
 
